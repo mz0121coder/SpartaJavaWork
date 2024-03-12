c7aa22209814b69dc0e3ac69e10b68c97f44ab8b
Mohammad Zafar <mzcoder7@gmail.com>
2024-03-12 15:15:59 +0000

    Greeter Tests passing

--- Week4/src/main/java/com/sparta/mz/basics/Greeter.java
+++ Week4/src/main/java/com/sparta/mz/basics/Greeter.java
@@ -1,18 +1,20 @@
 package com.sparta.mz.basics;
-//Read, Maintain, Test
+
 public class Greeter {
     public static void main(String[] args) {
         int timeOfDay = 10;
         System.out.println(getGreeting(timeOfDay));
     }
 
-    private static String getGreeting(int timeOfDay) {
-        if (timeOfDay >= 5 && timeOfDay <= 12) {
-            return ("Good morning!");
+    public static String getGreeting(int timeOfDay) {
+        if(timeOfDay < 5) {
+            return "It's early!";
+        } else if (timeOfDay >= 5 && timeOfDay < 12) {
+            return "Good morning!";
         } else if (timeOfDay >= 12 && timeOfDay <= 18) {
-            return ("Good afternoon!");
+            return "Good afternoon!";
         } else {
-            return ("Good evening!");
+            return "Good evening!";
         }
     }
 }
