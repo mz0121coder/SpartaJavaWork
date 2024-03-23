c8359887006ed3170e253860bb76065dcc61f4db
Mohammad Zafar <mzcoder7@gmail.com>
2024-03-23 13:32:31 +0000

    Calculate bonus in getTotalScore method

--- Week5/src/main/java/com/sparta/mz/advancedjunit/Bowling.java
+++ Week5/src/main/java/com/sparta/mz/advancedjunit/Bowling.java
@@ -9,7 +9,17 @@ public class Bowling {
         int total = 0;
         for (int i = 0; i < 10; i++) {
             score += getFrameScore(frames[i]);
+            if (frames[i].equals("X")) {
+                if (frames[i + 1].equals("X")) {
+                    bonus += getFrameScore(frames[i + 1]);
+                    bonus += getFrameScore(frames[i + 2].substring(0, 1));
+                } else {
+                    bonus += getFrameScore(frames[i + 1].substring(0, 2));
+                }
+            }
+            if (frames[i].endsWith("/")) bonus += getFrameScore(frames[i + 1].substring(0, 1));
         }
+        total += score + bonus;
         return total;
     }
 
@@ -17,7 +27,7 @@ public class Bowling {
         int frameScore = 0;
         for (int i = 0; i < frame.length(); i++) {
             char roll = frame.charAt(i);
-            if (Character.isDigit(roll)) frameScore += Integer.parseInt(String.valueOf(roll));
+            if (Character.isDigit(roll)) frameScore += Character.getNumericValue(roll);
             if (roll == '/') frameScore += (10 - frameScore);
             if (roll == 'X') frameScore += 10;
         }
